aea77420d6f2dc8b6f95dc349bdeb7afa3a6f2b0
Mike Haiduk <x29a100@gmail.com>
2024-12-09 00:00:37 +0100

    migrate to const

--- info.php
+++ info.php
@@ -3,7 +3,6 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/mfm-mining/utils.php";
 
 $domain = get_required(domain);
 $address = get_required(address);
-$gas_domain = get_required(gas_domain);
 
 $response[last_hash] = dataGet([mining, $domain, last_hash]) ?: "";
 $response[difficulty] = dataGet([mining, $domain, difficulty]) ?: 1;
