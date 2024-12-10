9b65ae943c2866abcee3cd79f1bd532c0416b903
Mike Haiduk <x29a100@gmail.com>
2024-12-10 16:38:09 +0100

    add navigate

--- utils.php
+++ utils.php
@@ -6,7 +6,7 @@ $round_seconds = 10;
 
 
 function getReward($domain) {
-    $round_reward_percent = 0.001;
+    $round_reward_percent = 0.0001;
     $token_balance = tokenBalance($domain, mining);
     return round($token_balance * $round_reward_percent, 2);
 }
